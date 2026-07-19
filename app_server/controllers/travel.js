355264f5d54d09d57a70cae0cbf659318fa518c2
Shaun Walsh <compguru910@gmail.com>
2026-07-19 13:37:36 -0500

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,10 +1,14 @@
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
+
+
 /* GET Homepage */
 const travel = (req, res) => 
 {
-    res.render('travel', { title: "Travlr Getaways"});
+    res.render('travel', { title: 'Travlr Getaways', trips});
 };
 
 module.exports = 
 {
-    travel
+     travel 
 }
--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var controller = require('../controllers/travel');
+var controller = require('../controllers/travel');  
 
 /* GET travel page */
-router.get('/', controller.travel);
+router.get('/', controller.travel);   
 
 module.exports = router;
