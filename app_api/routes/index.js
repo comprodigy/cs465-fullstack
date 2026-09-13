a2a59362d98aca2a5c670f68bf171f982359516f
Shaun Walsh <compguru910@gmail.com>
2026-09-13 15:02:41 -0800

    Pushing final changes

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,21 +2,25 @@ const express = require("express");
 const router = express.Router();
 
 const tripsController = require("../controllers/trips");
+const authController = require("../controllers/authentication");
 
 router.get("/test", (req, res) => {
   res.json({ message: "API routes are working" });
 });
 
+router.route("/login").post(authController.login);
+
 // GET + POST /api/trips
 router
   .route("/trips")
   .get(tripsController.tripsList)
-  .post(tripsController.tripsAddTrip);
+  .post(authenticateJWT, tripsController.tripsAddTrip);
 
 // GET + PUT /api/trips/:tripCode
 router
   .route("/trips/:tripCode")
   .get(tripsController.tripsFindByCode)
   .put(tripsController.tripsUpdateTrip);
+
 
 module.exports = router;
